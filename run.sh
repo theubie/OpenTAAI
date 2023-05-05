17719f9bb1b3f9d4e8cc41fb69dddad9f6b4b210
theubie <randell@nvcreativetechnology.com>
2023-05-05 01:58:29 -0500

    Fix run.sh to point to correct Linux locations

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ ! -f ./venv/Scripts/activate.bat ]; then
-    ./install.bat
+if [ ! -f ./venv/bin/activate ]; then
+    ./install.sh
 fi
 
-source ./venv/Scripts/activate.bat && python main.py --chat_file chat.txt --api_key_file api_key.txt --poll_interval 5 --context_file context.txt --custom_pronunciations_file custom_words.txt && deactivate
+source ./venv/bin/activate && python main.py --chat_file chat.txt --api_key_file api_key.txt --poll_interval 5 --context_file context.txt --custom_pronunciations_file custom_words.txt && deactivate
 
 echo "Press any key to continue..."
 read -n 1 -s
