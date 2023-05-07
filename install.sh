623e9500cf6a75d49e58a23ec6963223c406fd7a
theubie <randell@nvcreativetechnology.com>
2023-05-07 17:48:18 -0500

    Update README.md

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-python -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
+echo "Please select your preferred installation type:"
+echo "A. NVIDIA GPU (requires CUDA)"
+echo "B. CPU only"
+
+read -p "Enter your selection [A/B]: " choice
+
+if [ $choice == "A" ] || [ $choice == "a" ]
+then
+  pip install -r requirements.txt
+elif [ $choice == "B" ] || [ $choice == "b" ]
+then
+  pip install -r requirements-cpu.txt
+else
+  echo "Invalid choice. Please enter A or B."
+fi
+
 echo "Done!"
